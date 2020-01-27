6e692fd5618c0cb3c88090f5f5a060ddeb36628e
stefan hranek <hranekstefan@gmail.com>
2020-01-27 17:21:12 +0100

    Added svg icons for contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,6 +10,53 @@ export default class Contact extends Component {
                 <p>
                     I'm always open to collaborating and exploring new projects. Have an idea? Need some advice? Shoot me a message. I'd love to help!
                 </p>
+                <section>
+                    <a 
+                        href="https://github.com/stefanhranek"
+                        target="_blank"
+                    >
+                        <div>
+                            <div className="linkContainer">
+                                <img
+                                    src="./github.svg"
+                                    className="contactLogos"
+                                    alt="Github logo"
+                                />
+                                <h3>Github</h3>
+                            </div>
+                        </div>
+                    </a>
+                    <a 
+                        href="https://www.linkedin.com/in/stefanhranek/"
+                        target="_blank"
+                    >
+                        <div>
+                            <div className="linkContainer">
+                                <img
+                                    src="./linkedin.svg"
+                                    className="contactLogos"
+                                    alt="Github logo"
+                                />
+                                <h3>LinkedIn</h3>
+                            </div>
+                        </div>
+                    </a>
+                    <a 
+                        href="mailto:hranekstefan@gmail.com?subject=Let's work together! 😊&body=Hey Stefan! My name is Foobar. Would you like to grab a coffee & discuss projects?"
+                        target="_blank"
+                    >
+                        <div>
+                            <div className="linkContainer">
+                                <img
+                                    src="./gmail.svg"
+                                    className="contactLogos"
+                                    alt="Gmail logo"
+                                />
+                                <h3>Github</h3>
+                            </div>
+                        </div>
+                    </a> 
+                </section>
             </div>
         )
     }
