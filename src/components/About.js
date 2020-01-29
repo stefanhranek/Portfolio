b2da5f092583aac3e4f98b800ab444827c5381e9
stefan hranek <hranekstefan@gmail.com>
2020-01-29 18:24:27 +0100

    Updated console messsage

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ import AboutDetails from "./AboutDetails";
 
 export default class About extends Component {
   componentDidMount() {
-    console.log(`%c Welcome to my portfolio! Have an avocado 🥑`, 'padding: 20px; font-weight: bold; font-size: 30px;color: peachpuff; text-shadow: 3px 3px 0 #138086 , 6px 6px 0 #534666 , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 #CD7672 , 15px 15px 0 #EEB462 , 18px 18px 0 #DC8665 , 21px 21px 0 black');
+    console.log(`%c Welcome to my portfolio! Have an avocado 🥑`, 'padding: 20px 20px 40px 20px; font-weight: bold; font-size: 30px;color: peachpuff; text-shadow: 3px 3px 0 #138086 , 6px 6px 0 #534666 , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 #CD7672 , 15px 15px 0 #EEB462 , 18px 18px 0 #DC8665 , 21px 21px 0 black');
   }
 
   render() {
