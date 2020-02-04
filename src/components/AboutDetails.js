6049fabb134fded0e9b0bbaf359fcb05cb0a6712
stefan hranek <hranekstefan@gmail.com>
2020-02-04 11:57:49 +0100

    About - fixed anime gif sizing in details slide

--- src/components/AboutDetails.js
+++ src/components/AboutDetails.js
@@ -14,13 +14,13 @@ export default class ReactSlickDemo extends Component {
         };
         return (
           <Slider {...settings} className="AboutDetails">
-            <div >
+            <div className="aboutText">
               <p>I'm a web developer based in San Diego, California. I find joy in seeing ideas come to life, which is why I love creating web & mobile applications.</p>
             </div>
-            <div>
+            <div className="aboutText">
               <p>I love skateboarding - much of my free time goes to that. Other interests: surfing, climbing, scuba diving, table tennis, live music, board games, craft beer, coffee, dogs, and Barcelona ❤</p>
             </div>
-            <div>
+            <div className="aboutText">
               <p>I also admire anime. I've spent much of my life absorbing it, and it still makes me feel like I can take on the world. I draw inspiration from its dream-oriented nature and beautiful imagery.</p>
             </div>
             <div className='anime'>
