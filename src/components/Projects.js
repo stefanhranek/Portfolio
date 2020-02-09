46993b33f59314d692d11c40f5fbc95407779748
stefan hranek <hranekstefan@gmail.com>
2020-02-09 22:44:14 +0100

    Projects - added slide effect to project sections

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import ReactDOM from "react-dom";
 import "./../styles/App.scss";
+import Bounce from "react-reveal/Bounce";
 import { Link } from "react-scroll";
 
 export default class Projects extends Component {
@@ -32,64 +33,72 @@ export default class Projects extends Component {
         </Link>
 
         <div className="projectsList" id="projectsList">
-          <section>
-            <article>
-              <h1>Spot Check</h1>
-              <p>Map-based platform for finding & sharing skate spots.</p>
-            </article>
-            <a
-              href="https://spotcheck-react.herokuapp.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                className="imageBorder"
-                src="./../../spotCheckProject.png"
-                alt="Spot Check app"
-              />
-            </a>
-          </section>
-          <section  className="movieLogProject">
-            <a
-              href="https://m2-deploy-movielog.herokuapp.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                className="reversedImage"
-                src="./../../movieLogProject.png"
-                alt="Movie Log app"
-              />
-            </a>
-            <article className="reversedProject">
-              <h1>Movie Log</h1>
-              <p>
-                Keep track of the movies you've watched & the ones you wish to
-                see.
-              </p>
-            </article>
-          </section>
-          <section>
-            <article>
-              <h1>Jumper</h1>
-              <p>
-                2D JavaScript platformer game inspired by my favorite Super
-                Nintendo games.
-              </p>
-            </article>
-            <a
-              href="https://stefanhranek.github.io/jumper/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="lastProjectImage"
-            >
-              <img
-                className="imageBorder"
-                src="./../../jumperProject.png"
-                alt="Jumper app"
-              />
-            </a>
-          </section>
+          <Bounce left>
+            <section>
+              <article>
+                <h1>Spot Check</h1>
+                <p>Map-based platform for finding & sharing skate spots.</p>
+              </article>
+              <a
+                href="https://spotcheck-react.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="imageBorder"
+                  src="./../../spotCheckProject.png"
+                  alt="Spot Check app"
+                />
+              </a>
+            </section>
+          </Bounce>
+
+          <Bounce right>
+            <section className="movieLogProject">
+              <a
+                href="https://m2-deploy-movielog.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="reversedImage"
+                  src="./../../movieLogProject.png"
+                  alt="Movie Log app"
+                />
+              </a>
+              <article className="reversedProject">
+                <h1>Movie Log</h1>
+                <p>
+                  Keep track of the movies you've watched & the ones you wish to
+                  see.
+                </p>
+              </article>
+            </section>
+          </Bounce>
+
+          <Bounce left>
+            <section>
+              <article>
+                <h1>Jumper</h1>
+                <p>
+                  2D JavaScript platformer game inspired by my favorite Super
+                  Nintendo games.
+                </p>
+              </article>
+              <a
+                href="https://stefanhranek.github.io/jumper/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="lastProjectImage"
+              >
+                <img
+                  className="imageBorder"
+                  src="./../../jumperProject.png"
+                  alt="Jumper app"
+                />
+              </a>
+            </section>
+          </Bounce>
         </div>
       </div>
     );
