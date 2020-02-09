0c9787a4f7acb4a8879aa9238f4100a21ceab264
stefan hranek <hranekstefan@gmail.com>
2020-02-09 23:41:17 +0100

    Contact - added fade slide effect to icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,71 +1,80 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 // import ReactDOM from 'react-dom';
-import './../styles/App.scss';
+import Fade from "react-reveal/Fade";
+import "./../styles/App.scss";
 
 export default class Contact extends Component {
-    render() {
-        return (
-            <div className="page-section" id="contact">
-            <div className="contactContainer">
-                <header>Get in touch.</header>
-                <p>
-                    I'm always open to collaborating, sharing ideas, and learning new tricks & tips. Have an idea? Need some advice? Want to grab coffee? Shoot me a message. I'm happy to converse!
-                </p>
-                
-                <section className="bottomContactContainer">
-                    <a 
-                        href="https://github.com/stefanhranek"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <div className="singleLogoContainer">
-                            <div className="linkContainer">
-                                <img
-                                    src="./github.svg"
-                                    className="contactLogos"
-                                    alt="Github"
-                                />
-                                <h3>Github</h3>
-                            </div>
-                        </div>
-                    </a>
-                    <a 
-                        href="mailto:hranekstefan@gmail.com?subject=Let's work together! 😊&body=Hey Stefan! My name is Foobar. Would you like to grab a coffee and discuss projects?"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <div className="singleLogoContainer">
-                            <div className="linkContainer">
-                                <img
-                                    src="./gmail.svg"
-                                    className="contactLogos"
-                                    alt="Gmail"
-                                />
-                                <h3>Gmail</h3>
-                            </div>
-                        </div>
-                    </a> 
-                    <a 
-                        href="https://www.linkedin.com/in/stefanhranek/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <div className="singleLogoContainer">
-                            <div className="linkContainer">
-                                <img
-                                    src="./linkedin.svg"
-                                    className="contactLogos"
-                                    alt="Github"
-                                />
-                                <h3>LinkedIn</h3>
-                            </div>
-                        </div>
-                    </a>
-                </section>
-            
+  render() {
+    return (
+      <div className="page-section" id="contact">
+        <div className="contactContainer">
+          <header>Get in touch.</header>
+          <p>
+            I'm always open to collaborating, sharing ideas, and learning new
+            tricks & tips. Have an idea? Need some advice? Want to grab coffee?
+            Shoot me a message. I'm happy to converse!
+          </p>
+
+          <section className="bottomContactContainer">
+            <Fade left>
+              <a
+                href="https://github.com/stefanhranek"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="singleLogoContainer">
+                  <div className="linkContainer">
+                    <img
+                      src="./github.svg"
+                      className="contactLogos"
+                      alt="Github"
+                    />
+                    <h3>Github</h3>
+                  </div>
                 </div>
-            
-            </div>
-        )
-    }
+              </a>
+            </Fade>
+
+            <Fade bottom>
+              <a
+                href="mailto:hranekstefan@gmail.com?subject=Let's work together! 😊&body=Hey Stefan! My name is Foobar. Would you like to grab a coffee and discuss projects?"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="singleLogoContainer">
+                  <div className="linkContainer">
+                    <img
+                      src="./gmail.svg"
+                      className="contactLogos"
+                      alt="Gmail"
+                    />
+                    <h3>Gmail</h3>
+                  </div>
+                </div>
+              </a>
+            </Fade>
+
+            <Fade right>
+              <a
+                href="https://www.linkedin.com/in/stefanhranek/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="singleLogoContainer">
+                  <div className="linkContainer">
+                    <img
+                      src="./linkedin.svg"
+                      className="contactLogos"
+                      alt="Github"
+                    />
+                    <h3>LinkedIn</h3>
+                  </div>
+                </div>
+              </a>
+            </Fade>
+          </section>
+        </div>
+      </div>
+    );
+  }
 }
