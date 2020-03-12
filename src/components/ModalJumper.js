a47ba83ed56409ca59edd337f1d8bbc9caee96c5
stefan hranek <hranekstefan@gmail.com>
2020-03-12 14:51:59 -0700

    New component files for other project modals

---
+++ src/components/ModalJumper.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import "./../styles/App.scss";
+import PropTypes from 'prop-types';
+
+class ModalJumper extends React.Component {
+  render() {
+    // Render nothing if the "show" prop is false
+    if(!this.props.show) {
+      return null;
+    }
+
+    return (
+      <div className="backdrop">
+        <div className="modal">
+          {this.props.children}
+
+          <div className="footer">
+            <button className="modalEscapeButton" onClick={this.props.onClose}>
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ModalJumper.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  show: PropTypes.bool,
+  children: PropTypes.node
+};
+
+export default ModalJumper;
