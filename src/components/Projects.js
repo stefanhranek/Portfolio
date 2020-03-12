e9a95002e6ef16aac8be02d0dcbcad6a1d7916e1
stefan hranek <hranekstefan@gmail.com>
2020-03-11 23:21:19 -0700

    New phone image for Spot Check modal

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,58 +26,63 @@ export default class Projects extends Component {
             <section>
               <h1>Spot Check</h1>
               <h2>Features:</h2>
-                <ul>
-                  <li>User authentication with private routes</li>
-                  <li>Sign up & Log in</li>
-                  <li>Form error feedback</li>
-                  <li>Edit user profile</li>
-                  <li>Choose point on global map to create a new skate spot</li>
-                  <li>View details of skate spot</li>
-                  <li>Add existing spots to "Favorites" list</li>
-                  <li>Number of user contributions displayed on profile</li>
-                  <li>Mobile-first design, fully responsive</li>
-                </ul>
+              <ul>
+                <li>User authentication with private routes</li>
+                <li>Sign up & Log in</li>
+                <li>Form error feedback</li>
+                <li>Edit user profile</li>
+                <li>Choose point on global map to create a new skate spot</li>
+                <li>View details of skate spot</li>
+                <li>Add existing spots to "Favorites" list</li>
+                <li>Number of user contributions displayed on profile</li>
+                <li>Mobile-first design, fully responsive</li>
+              </ul>
               <h2>Technologies:</h2>
-                <ul>
-                  <li>React</li>
-                  <li>JavaScript</li>
-                  <li>Node.js</li>
-                  <li>Express</li>
-                  <li>MongoDB</li>
-                  <li>CSS</li>
-                  <li>REST API</li>
-                  <li>Axios</li>
-                  <li>Mapbox API</li>
-                  <li>Heroku</li>
-                </ul>
-              <a
-                className="modalLink"
-                href="https://github.com/stefanhranek/Spot-Check-Client"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub (Client)
-              </a>
-              <a
-                className="modalLink"
-                href="https://github.com/stefanhranek/Spot-Check-Server"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub (Server)
-              </a>
-              <a
-                className="modalLink"
-                href="https://spotcheck-react.herokuapp.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Deploy
-              </a>
-
+              <ul>
+                <li>React</li>
+                <li>JavaScript</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>MongoDB</li>
+                <li>CSS</li>
+                <li>REST API</li>
+                <li>Axios</li>
+                <li>Mapbox API</li>
+                <li>Heroku</li>
+              </ul>
+              <div className="modalLinkContainer">
+                <a
+                  className="modalLink"
+                  href="https://github.com/stefanhranek/Spot-Check-Client"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub (Client)
+                </a>
+                <a
+                  className="modalLink"
+                  href="https://github.com/stefanhranek/Spot-Check-Server"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub (Server)
+                </a>
+                <a
+                  className="modalLink"
+                  href="https://spotcheck-react.herokuapp.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Deploy
+                </a>
+              </div>
             </section>
             <section>
-
+              <img
+                className="imageBorder"
+                src="./../../spotCheckPhone.png"
+                alt="Spot Check app"
+              />
             </section>
           </Modal>
 
