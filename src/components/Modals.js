e5fc6f1760f6e38cb54961d76c30e3bc913d18e6
stefan hranek <hranekstefan@gmail.com>
2020-03-11 22:18:03 -0700

    Modal escape button positioned top right

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -36,7 +36,7 @@ class Modal extends React.Component {
           {this.props.children}
 
           <div className="footer">
-            <button onClick={this.props.onClose}>
+            <button className="modalEscapeButton" onClick={this.props.onClose}>
               Close
             </button>
           </div>
